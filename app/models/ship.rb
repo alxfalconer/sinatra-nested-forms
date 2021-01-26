7199d4159ea9f6909dc5f421a0a852b0a5469e48
Alexander <al.x.falconer@gmail.com>
2021-01-26 15:10:21 -0500

    commit tree 547296b70bc380c69e7b7534e04f2bbba172479b

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
-end
+    attr_accessor :name, :type, :booty
+  
+    class << self
+      attr_accessor :all
+    end
+  
+    @all = []
+  
+    def initialize(args)
+      @name = args[:name]
+      @type = args[:type]
+      @booty = args[:booty]
+  
+      self.class.all << self
+    end
+  
+    def self.clear
+      @all = []
+    end
+  end
