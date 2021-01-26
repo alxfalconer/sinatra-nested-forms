73279ab8a754761a1492bed2e78d9829c8a75336
Alexander <al.x.falconer@gmail.com>
2021-01-26 15:10:28 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
-end
+    attr_accessor :name, :weight, :height
+  
+    class << self
+      attr_accessor :all
+    end
+  
+    @all = []
+  
+    def initialize(args)
+      @name = args[:name]
+      @weight = args[:weight]
+      @height = args[:height]
+  
+      self.class.all << self
+    end
+  
+    def self.clear
+      @all = []
+    end
+  end
